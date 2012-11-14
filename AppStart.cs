c4aa96a20fac71fa45056f617d883a1150e7309b
Creg Bradley <cbraldey@daptiv.com>
2012-11-14 13:55:17 -0800

    Add initial implementation

---
+++ AppStart.cs
@@ -0,0 +1,12 @@
+﻿using ServiceStack.WebHost.Endpoints;
+
+namespace ServiceStack.Plugin.Yaml
+{
+	public class AppStart
+	{
+		public static void Start()
+		{
+			EndpointHost.AddPlugin(new YamlFormat());
+		}
+	}
+}
---
+++ YamlFormat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using ServiceStack.Common.Web;
+using ServiceStack.WebHost.Endpoints;
+using YamlDotNet.RepresentationModel.Serialization;
+
+namespace ServiceStack.Plugin.Yaml
+{
+	public class YamlFormat : IPlugin
+	{
+		public void Register(IAppHost appHost)
+		{
+			appHost.ContentTypeFilters.Register(ContentType.Yaml,
+			                                    (reqCtx, res, stream) => Serialize(stream, res),
+			                                    Deserialize);
+		}
+
+		private void Serialize(Stream stream, object res)
+		{
+			var yamlSerializer = new YamlSerializer(res.GetType());
+			var writer = new StreamWriter(stream);
+			yamlSerializer.Serialize(writer, res);
+		}
+
+		private object Deserialize(Type type, Stream fromStream)
+		{
+			var yamlSerializer = new YamlSerializer(type);
+			var reader = new StreamReader(fromStream);
+			return yamlSerializer.Deserialize(reader);
+		}
+	}
+}
